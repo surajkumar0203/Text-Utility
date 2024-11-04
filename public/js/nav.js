0d884e0d6264b036b072571246b7598776b047c8
surajkumar0203 <kumarsuraj0203@gmail.com>
2024-11-04 14:34:31 +0530

    some change

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,19 @@
 "use strict"
 let hum=document.getElementById('humburger');
 let flag=0
+
 hum.addEventListener('click',function(){
     let ul=document.getElementById('ul');
     
     if(flag==0){
     
-        ul.classList.add('translate-y-0')
+        ul.className="pb-2 mx-3 sm:flex sm:justify-end  sm:absolute sm:right-2 sm:top-0  sm:text-white sm:bg-none sm:mr-20 sm:translate-y-0 relative top-4 rounded-lg bg-gradient-to-r from-purple-400 to-blue-400  translate-y-52 transition-all duration-200"
         flag=1
         
     }
     else{
-        ul.classList.remove('translate-y-0')
+      
+        ul.className="pb-2 mx-3 sm:flex sm:justify-end  sm:absolute sm:right-2 sm:top-0  sm:text-white sm:bg-none sm:mr-20 sm:translate-y-0 relative top-4 rounded-lg bg-gradient-to-r from-purple-400 to-blue-400  -translate-y-52 transition-all duration-200"
         flag=0
     }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html","./src/**/*.{html,js}"],
+  content: ["./*.html","./src/**/*.{html,js}"],
+
   theme: {
     extend: {
       content: {
